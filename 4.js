0ec8f45aae17c8e60ab93132f929ff29691d3df1
jiyoung <jen.jyseo@gmail.com>
2022-09-24 13:57:19 +0900

    add:: 4번 문제

--- 4.js
+++ 4.js
@@ -50,9 +50,13 @@ function solution(participant, completion) {
   return answer;
 }
 
-solution(["leo", "kiki", "eden"], ["eden", "kiki"]);
-solution(
-  ["marina", "josipa", "nikola", "vinko", "filipa"],
-  ["josipa", "filipa", "marina", "nikola"]
+console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"]));
+console.log(
+  solution(
+    ["marina", "josipa", "nikola", "vinko", "filipa"],
+    ["josipa", "filipa", "marina", "nikola"]
+  )
 );
-solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]);
+console.log(
+  solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+);
