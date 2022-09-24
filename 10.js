20f7a740731fc055ebb43fc9c0c94a289d256b3a
jiyoung <jen.jyseo@gmail.com>
2022-09-24 15:08:50 +0900

    add:: 10번 문제

--- 10.js
+++ 10.js
@@ -12,8 +12,8 @@ arr은 길이 1 이상인 배열입니다.
     [10]	   [-1]
  */
 function solution(arr) {
-  let filterdArr = arr.splice(arr.indexOf(Math.min(...arr)), 1);
-  var answer = arr.length ? arr : [-1];
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+  var answer = arr.length <= 1 ? [-1] : arr;
   return answer;
 }
 
