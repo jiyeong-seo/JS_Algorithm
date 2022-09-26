eec44d2cd44c8e0e36f84a6c6efb6c7c642ba95e
jiyoung <jen.jyseo@gmail.com>
2022-09-26 18:25:04 +0900

    add:: 21번 문제

--- 21.js
+++ 21.js
@@ -44,3 +44,39 @@ function solution(nums) {
 
 solution([1, 2, 3, 4]);
 solution([1, 2, 7, 6, 4]);
+
+function primecheck(n) {
+  for (var i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i !== 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function primecheck(n) {
+  for (var i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function solution(nums) {
+  var cnt = 0;
+  for (var i = 0; i < nums.length - 2; i++) {
+    for (var j = i + 1; j < nums.length - 1; j++) {
+      for (var w = j + 1; w < nums.length; w++) {
+        cnt++;
+        if (primecheck(nums[i] + nums[j] + nums[w])) {
+          cnt--;
+        }
+      }
+    }
+  }
+  return cnt;
+}
+
+solution([1, 2, 3, 4]);
+solution([1, 2, 7, 6, 4]);
